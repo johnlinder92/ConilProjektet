9f3182dbb9cccc0f12d77af84349542e72805b99
johnlinder92 <john.artur.linder@gmail.com>
2019-10-25 11:04:30 +0200

    lagt till test som bokar i kalendern och sedan tar bort bokningen i adminen.

--- Test/CalendarTest.java
+++ Test/CalendarTest.java
@@ -26,7 +26,9 @@ public class CalendarTest {
     JavascriptExecutor js;
     @Before
     public void setUp() {
-        driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("disable-infobars");
+        driver = new ChromeDriver(options);
         js = (JavascriptExecutor) driver;
         vars = new HashMap<String, Object>();
     }
@@ -37,11 +39,11 @@ public class CalendarTest {
         driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
         driver.manage().window().setSize(new Dimension(1259, 728));
 
-        {
+
             driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
             WebElement element2 = driver.findElement(By.id("booking_form_div1"));
             ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", element2);
-        }
+
         driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
         driver.findElement(By.id("name1")).click();
         driver.findElement(By.id("name1")).sendKeys("John Linder");
@@ -54,8 +56,49 @@ public class CalendarTest {
         ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", element3);
         driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
         driver.findElement(By.linkText("»")).click();
+        WebElement element6= driver.findElement(By.id("bklnk1"));
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", element6);
         driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
         driver.findElement(By.linkText("22")).click();
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+                WebElement element5= driver.findElement(By.id("bklnk1"));
+                driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+                ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", element5);
+                driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+                driver.findElement(By.tagName("button")).click();
+                driver.findElement(By.tagName("button")).click();
+                driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+
+               WebElement scrolla = driver.findElement(By.id("bklnk1"));
+                ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", scrolla);
+
+
+
+
+
+        driver.get("https://dev.conil.se/wp-login.php");
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+        WebElement elementos = driver.findElement(By.id("user_login"));
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+        elementos.sendKeys("admin");
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+        driver.findElement(By.id("user_pass")).sendKeys("password");
+        driver.findElement(By.id("wp-submit")).click();
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+        driver.findElement(By.linkText("Bokningskalender 2")).click();
+        driver.findElement(By.cssSelector(".glyphicon-align-justify")).click();
+        driver.findElement(By.cssSelector(".col-xs-4 .wpbc-no-margin > .wpbc-no-margin")).click();
+        driver.findElement(By.cssSelector(".nav-tab:nth-child(2) > .nav-tab-text")).click();
+        driver.findElement(By.cssSelector(".input-group-btn:nth-child(3) .in-button-text")).click();
+        assertThat(driver.switchTo().alert().getText(), is("Vill du verkligen radera valda bokningar?"));
+        driver.switchTo().alert().accept();
+
+
+
+
+
 
     }
 }
